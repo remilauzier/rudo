bbb085a2d3169debfa5327709b8e3f502a505a51
Rémi Lauzier <remilauzier@protonmail.com>
2021-04-22 02:28:48 -0400

    Mention anssi rule in comment and refuse mem_forget and meaningless shadowing
    Signed-off-by: Rémi Lauzier <remilauzier@protonmail.com>

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ like group membership and validity of the account
 #![deny(
 rustdoc,                 // rustdoc is for verifying the validity of some part of the documentation
 future_incompatible,     // future_incompatible is to ensure to be ready for future version of rust
-nonstandard_style,       // nonstandard_style is for verifying that Rudo respect convention
+nonstandard_style,       // nonstandard_style is for verifying that Rudo respect convention. Rule LANG-NAMING anssi
 rust_2018_compatibility, // rust_2018_compatibility is for forcing the 2018 convention as some small thing can be ignored by compiler
 rust_2018_idioms,        // rust_2018_idioms is for forcing the 2018 convention as some small thing can be ignored by compiler
 warnings,                // Switch all warning to deny, to force better code by default
@@ -50,14 +50,17 @@ clippy::wrong_pub_self_convention,     // Be consistent and follow convention fo
 clippy::needless_borrow,               // Removed needless borrow in code for better clarity. Nursery
 clippy::use_self,                      // Use self when its possible instead of given the name of a struct or other type everywhere. Nursery
 clippy::useless_let_if_seq,            // Prefer idiomatic rust for clarity in code . Nursery
-clippy::expect_used,                   // Refuse .expect() since Rudo is production code
+clippy::expect_used,                   // Refuse .expect() since Rudo is production code. Rule LANG-NOPANIC anssi
 clippy::filetype_is_file,              // Prefer !FileType::is_dir() instead of is_file() since it can have problem with special file or symlink
 clippy::get_unwrap,                    // Prefer [0] instead of .get(0).unwrap when sure it's impossible to fail since it's more concise and clear
-clippy::unwrap_in_result,              // Refuse to change a recoverable error in a non-recoverable one
-clippy::unwrap_used,                   // Refuse .unwrap() since Rudo is production code
+clippy::unwrap_in_result,              // Refuse to change a recoverable error in a non-recoverable one. Rule LANG-NOPANIC anssi
+clippy::unwrap_used,                   // Refuse .unwrap() since Rudo is production code. Rule LANG-NOPANIC anssi
 clippy::let_underscore_must_use,       // It’s better to explicitly handle the value of a #[must_use] expr
 clippy::cognitive_complexity,          // Verify the complexity of a function to not be further than 25. Can be change later if necessary.
-clippy::else_if_without_else           // Follow MISRA-C:2004 Rule 14.10 and be defensive in the code. Take care of 0.01% chance an error happens.
+clippy::else_if_without_else,          // Follow MISRA-C:2004 Rule 14.10 and be defensive in the code. Take care of 0.01% chance an error happens.
+clippy::mem_forget,                    // Don't use mem_forget since it can cause memory leaks. Rule MEM-FORGET and Recommendation MEM-FORGET-LINT anssi
+clippy::shadow_reuse,                  // Ensure the code is easy to follow by refusing meaningless shadowing
+clippy::shadow_same                    // Ensure the code is easy to follow by refusing meaningless shadowing
 )]
 // Authorized redundant else to conform to MISRA-C:2004 Rule 14.10 and to not conflict with clippy::else_if_without_else
 #![allow(clippy::redundant_else)]
